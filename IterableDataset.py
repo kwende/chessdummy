706ffe1822af65dba426ab691ded065a1c2ed5f1
Ben Rush <brush@ocuvera.com>
2023-08-05 16:23:50 -0500

    Starting to work.

--- IterableDataset.py
+++ IterableDataset.py
@@ -3,7 +3,7 @@ from itertools import cycle, islice
 
 import sqlite3
 
-class IteratableDataset(IterableDataset):
+class ChessDataset(IterableDataset):
     def __init__(self, db_path):
         self.db_path = db_path
         self.db_connection = sqlite3.connect(db_path)
@@ -26,7 +26,7 @@ class IteratableDataset(IterableDataset):
 
 if __name__ == "__main__":
     
-    ds = IteratableDataset('e:/chess.db')
+    ds = ChessDataset('e:/chess.db')
     loader = DataLoader(ds, batch_size=4)
 
     for batch in islice(loader, 8):
--- train.py
+++ train.py
@@ -3,10 +3,84 @@ import torch.nn as nn
 import torchvision.transforms as transforms
 import torchvision.datasets as dsets
 import numpy as np
+import IterableDataset
 
-class Network(nn.Module):
+class CNN(nn.Module):
 
     def __init__(self):
-        super(Network, self).__init__()
+        super(CNN, self).__init__()
+        self.cnn1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=2, padding=2)
+        self.cnn2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=2, padding=2)
+        self.cnn3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=2, padding=2)
+        self.fc1 = nn.Linear(256 * 8 * 8, 256)
+        self.fc2 = nn.Linear(256, 2)
+
+    def forward(self, x):
+        x = self.cnn1(x)
+        x = torch.relu(x)
+        x = self.cnn2(x)
+        x = torch.relu(x)
+        x = self.cnn3(x)
+        x = torch.relu(x)
+        x = self.fc1(x)
+        x = torch.relu(x)
+        x = self.fc2(x)
+        return x
+
+model = CNN()
+
+criterion = nn.CrossEntropyLoss()
+learning_rate = 0.1
+optimizer = torch.optim.SGD(model.parameters(), lr = learning_rate)
+
+chess_dataset = IterableDataset.ChessDataset("e:/chess.db")
+
+train_loader = torch.utils.data.DataLoader(dataset=chess_dataset, batch_size=100)
+test_loader = torch.utils.data.DataLoader(dataset=chess_dataset, batch_size=5000)
+
+# Train the model
+
+n_epochs=3
+cost_list=[]
+accuracy_list=[]
+N_test=5000
+COST=0
+
+def train_model(n_epochs):
+    for epoch in range(n_epochs):
+        COST=0
+        for x, y in train_loader:
+            optimizer.zero_grad()
+            z = model(x)
+            loss = criterion(z, y)
+            loss.backward()
+            optimizer.step()
+            COST+=loss.data
+        
+        cost_list.append(COST)
+        correct=0
+        #perform a prediction on the validation  data  
+        for x_test, y_test in test_loader:
+            z = model(x_test)
+            _, yhat = torch.max(z.data, 1)
+            correct += (yhat == y_test).sum().item()
+        accuracy = correct / N_test
+        accuracy_list.append(accuracy)
+     
+train_model(n_epochs)
+
+# def define(self):
+#         input_layer= Input(shape=(8,8,12))
+#         x = Conv2D(filters=64,kernel_size = 2,strides = (2,2))(input_layer)
+#         x = Conv2D(filters=128,kernel_size=2,strides = (2,2))(x)
+#         x = Conv2D(filters=256,kernel_size=2,strides = (2,2))(x)
+#         x = Flatten()(x)
+
+#         x = Dense(4096,activation = 'softmax')(x)
+#         output = Reshape((1,64,64))(x)
+
+#         model = Model(inputs=input_layer,outputs=output)
+#         model.compile(optimizer = self.optimzier,loss = self.loss)
+#         self.model = model
 
         
