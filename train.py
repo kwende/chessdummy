57a0381d5e60f406029f2f7d1d39c9800b5bc13c
Ben Rush <brush@ocuvera.com>
2023-09-16 17:27:52 -0500

    Sep15_10-38-50_BRUSH-WN10

--- train.py
+++ train.py
@@ -13,10 +13,10 @@ class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
         # NOTE: taking it down to 2 convd layers made it bad. But, removing the # of channels seemed to help per layer
-        self.cnn1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=2, padding=2)
-        self.cnn2 = nn.Conv2d(in_channels=64, out_channels=96, kernel_size=2, padding=2)
-        self.cnn3 = nn.Conv2d(in_channels=96, out_channels=128, kernel_size=2, padding=2)
-        self.cnn4 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=2, padding=2)
+        self.cnn1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=4, padding=2)
+        self.cnn2 = nn.Conv2d(in_channels=64, out_channels=96, kernel_size=4)
+        self.cnn3 = nn.Conv2d(in_channels=96, out_channels=128, kernel_size=4)
+        self.cnn4 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=2)
         self.global_pooling = nn.AdaptiveAvgPool2d(1)
         self.fc1 = nn.Linear(256, 10)
         self.fc2 = nn.Linear(10, 64)
@@ -38,11 +38,16 @@ class CNN(nn.Module):
         return x
 
 model = CNN()
+
+#cp = torch.load('D:/repos/chessdummy/best.ckpt')
+
+#model.load_state_dict(cp['model_state_dict'])
 model.cuda()
 
 criterion = nn.CrossEntropyLoss()
 learning_rate = 0.01
 optimizer = torch.optim.SGD(model.parameters(), lr = learning_rate)
+#optimizer.load_state_dict(cp['optimizer_state_dict'])
 
 chess_dataset = IterableDataset.ChessDataset("e:/chess.db")
 
@@ -61,6 +66,7 @@ def train_model(n_epochs):
     for epoch in range(n_epochs):
         #COST=0
         best_cost = -1.0
+        print("Running...")
         for x, y in train_loader:
             x = x.to('cuda')
             y = y.to('cuda')
@@ -75,10 +81,12 @@ def train_model(n_epochs):
             cost = loss.data
             if best_cost == -1 or cost < best_cost:
                 best_cost = cost
+                print("saving...")
                 torch.save({
                     'model_state_dict': model.state_dict(),
                     'optimizer_state_dict': optimizer.state_dict(),
                 }, "Best.ckpt")
+                print("...saved model")
         
         #cost_list.append(COST)
         # correct=0
